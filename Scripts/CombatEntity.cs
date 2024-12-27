dc2dac7ff5c70353829ed82ffd1c6e94a4096d29
ciprianmunteanu <justashadownfg@gmail.com>
2024-12-28 01:16:01 +0200

    Added health bar

--- Scripts/CombatEntity.cs
+++ Scripts/CombatEntity.cs
@@ -3,6 +3,9 @@ using Godot;
 public partial class CombatEntity : Node2D
 {
     [Export]
+    public ProgressBar HealthBar { get; set; }
+
+    [Export]
     public int Speed { get; set; }
     [Export]
     public int MaxHealth { get; set; }
@@ -10,4 +13,10 @@ public partial class CombatEntity : Node2D
     public int CurrentHealth { get; set; }
 
     public virtual void TakeTurn() { }
+
+    public override void _Ready()
+    {
+        HealthBar.Value = CurrentHealth / MaxHealth;
+        Speed = 1;
+    }
 }
