98b50d561f5cefe8f3d29b817e60be35c04b526e
ciprianmunteanu <justashadownfg@gmail.com>
2024-12-28 13:01:10 +0200

    Enemies play an animation when attacking the player

--- Scripts/EnemyCombatEntity.cs
+++ Scripts/EnemyCombatEntity.cs
@@ -4,9 +4,33 @@ using System.Diagnostics;
 
 public partial class EnemyCombatEntity : CombatEntity
 {
+    private AnimatedSprite2D attackAnimation;
+
+    private void CreateAnimation()
+    {
+        attackAnimation = new AnimatedSprite2D();
+        attackAnimation.SpriteFrames = GD.Load<SpriteFrames>("res://Assets/Attack_animation/AttackAnimation.tres");
+        attackAnimation.Position = PlayerCombatEntity.Instance.Position;
+        attackAnimation.Scale = new Vector2(4, 4);
+        GetTree().Root.AddChild(attackAnimation);
+        attackAnimation.AnimationLooped += OnAnimationFinished;
+    }
+
     public override void TakeTurn()
     {
+        if(attackAnimation == null)
+        {
+            CreateAnimation();
+        }
+        attackAnimation.Show();
+        attackAnimation.Play();
+    }
+
+    private void OnAnimationFinished()
+    {
         PlayerCombatEntity.Instance.TakeDamage(1);
+        attackAnimation.Pause();
+        attackAnimation.Hide();
         TurnManager.Instance.PassTurn();
     }
 }
