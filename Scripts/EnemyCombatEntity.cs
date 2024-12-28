8d0c5b2d9f76075cb5c121c36ad5d2e49b8145f0
ciprianmunteanu <justashadownfg@gmail.com>
2024-12-28 13:03:25 +0200

    Add the animation as child of the enemy for garbage collection

--- Scripts/EnemyCombatEntity.cs
+++ Scripts/EnemyCombatEntity.cs
@@ -9,10 +9,10 @@ public partial class EnemyCombatEntity : CombatEntity
     private void CreateAnimation()
     {
         attackAnimation = new AnimatedSprite2D();
+        AddChild(attackAnimation);
         attackAnimation.SpriteFrames = GD.Load<SpriteFrames>("res://Assets/Attack_animation/AttackAnimation.tres");
-        attackAnimation.Position = PlayerCombatEntity.Instance.Position;
-        attackAnimation.Scale = new Vector2(4, 4);
-        GetTree().Root.AddChild(attackAnimation);
+        attackAnimation.GlobalPosition = PlayerCombatEntity.Instance.Position;
+        attackAnimation.GlobalScale = new Vector2(4, 4);
         attackAnimation.AnimationLooped += OnAnimationFinished;
     }
 
